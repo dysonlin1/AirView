ebee95fedf8774b1d56e658a9e40aeb850def6f1
林湧森 (Dyson Lin) <dysonlin@gmail.com>
2017-05-31 18:21:09 +0800

    AirLog V1.0.4
    write an empty data to make Bokeh plot a break point

--- AirView.py
+++ AirView.py
@@ -6,14 +6,13 @@ from bokeh.io import curdoc
 from bokeh.layouts import row, column, widgetbox
 from bokeh.models import ColumnDataSource
 from bokeh.models.widgets import Slider, TextInput
+from bokeh.models import Button
 import pandas as pd
 from bokeh.plotting import figure, output_file, show, output_notebook
 from bokeh.models import DatetimeTickFormatter
 
 graph_title = 'AirView V4.0.7'
 csv_file_name = '2017-05-16 AirView.csv'
-df = pd.read_csv(csv_file_name, names=['Time', 'Air Voltage (mV)'], 
-                 parse_dates=['Time'])
 
 p = figure(plot_width=900, plot_height=500, x_axis_type="datetime",
         title=graph_title, 
@@ -32,13 +31,19 @@ p.xaxis[0].formatter = DatetimeTickFormatter(
         microseconds = '%H:%M:%S'
         )
 
-p.line(df['Time'], df['Air Voltage (mV)'])
+df = pd.read_csv(csv_file_name, names=['Time', 'Air Voltage (mV)'], 
+                 parse_dates=['Time'])
+r = p.line(df['Time'], df['Air Voltage (mV)'])
+
+ds = r.data_source
+
+# callback for update graph
 
 
+# refresh graph when Refresh button is pressed
+button = Button(label="Refresh")
 
+# put the button and plot in a layout and add to the document 
+curdoc().add_root(column(button, p))
 
-
-
-    
-curdoc().add_root(p)
 curdoc().title = graph_title
